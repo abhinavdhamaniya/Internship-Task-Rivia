1d32d5789855c877ff101b922811320ed7a00f0f
abhinavdhamaniya <46796268+abhinavdhamaniya@users.noreply.github.com>
2020-10-07 18:14:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const axios = require('axios').default;
+var express = require('express');
+var app = express();
+const jsStringify = require('js-stringify');
+
+var result;
+
+app.engine('pug', require('pug').__express)
+app.set('view engine', 'pug');  //to use the pug engine
+
+app.set('views', './views'); //views are avialable in views folder
+
+// Make a request for a user with a given ID
+axios.get('https://21823d1455a39718f9478e4af6e515ea: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@samplestorebyabhinavd.myshopify.com/admin/api/2020-10/orders.json?status=any')
+  .then(function (response) {
+    // handle success
+    //console.log(response.data);
+    result= response.data;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
+app.get('/', function(req,res)
+{
+    res.render('homepage', {jsStringify, orders: result.orders});
+});
+  
+
+
+var server = app.listen(process.env.PORT, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
